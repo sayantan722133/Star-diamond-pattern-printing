741a4ea7fbe3c78a5dd3f07646ebf2ffe3f4f7b8
sayantan722133 <85750945+sayantan722133@users.noreply.github.com>
2021-06-12 14:02:46 +0530

    Add files via upload

---
+++ Dimond pattern printing.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{   
+    int n,i,j,k;
+    printf("Enter Number of Rows: ");
+    scanf("%d", &n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n-i;j++)
+        {
+            printf("  ");
+        }
+        for(k=1;k<=((2*i)-1);k++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+     for(i=(n-1);i>=1;i--)
+    {
+        for(j=1;j<=n-i;j++)
+        {
+            printf("  ");
+        }
+        for(k=1;k<=((2*i)-1);k++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    printf("\nDone by Sayantan Mukhopadhyay \nRoll no - 10800120105");
+    return 0;
+}
